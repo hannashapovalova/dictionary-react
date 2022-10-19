2594e485237034c8f8f5f49224b1e056a9c42342
hannashapovalova <hannalova1991@gmail.com>
2022-10-19 16:12:18 +0100

    Added Fontawsome

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import './Phonetics.css';
+
 
 export default function Phonetics(props) {
     return (
         <div>
           <a
+           className="sound"
            href={props.phonetic.audio}
            target="_blank"
            rel="noreferrer">
             Listen
+            <i class="fa-solid fa-play"></i>
           </a>
-          <p>
+          <p className="transcription">
             {props.phonetic.text}
           </p>
         </div>
-
     );
 }
